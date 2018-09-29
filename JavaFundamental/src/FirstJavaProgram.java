edf0e736c4f1dc10d12ffe9b2879c9e9ca27ad97
kurtw29 <ckurt37@gmail.com>
2018-09-28 21:50:47 -0700

    first JavaStack commit

---
+++ JavaFundamental/src/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+
+public class FirstJavaProgram {
+	static String name = "Kurt W";
+	static int age = 12;
+	static String city = "San Jose";
+	static String state = "CA";
+	public static void main(String[] args) {
+		System.out.println("My name is "+name+".\nI am "+age+" years old. \nMy hometown is "+city+", "+state+".");
+	}
+}
