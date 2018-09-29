ac79388ce65b6b21152005b691cb862aee05c796
kurtw29 <ckurt37@gmail.com>
2018-09-29 13:06:20 -0700

    gitinore & Pythagorean

---
+++ JavaFundamental/src/PythagoreanResult.java
@@ -0,0 +1,8 @@
+
+public class PythagoreanResult {
+	public static void main(String[] args) {
+		Pythagorean h = new Pythagorean();
+//		Double hResult = h.calculateHypotenuse(int a, int b);
+		System.out.println("The hypotenus of 4 and 5 is: "+h.calculateHypotenuse(5, 4));
+	}
+}
